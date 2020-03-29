1e1e97bd8619cfa120f6ae91991ac038303da777
Hugo <bombix34@gmail.com>
2020-03-29 17:07:40 +0200

    ufps nul + algo of life

--- Assets/Scripts/ChunkManager.cs
+++ Assets/Scripts/ChunkManager.cs
@@ -4,27 +4,34 @@ using UnityEngine;
 
 public class ChunkManager : MonoBehaviour
 {
-    private ListOptim<GameObject> cubes;
+    [SerializeField]
+    private ChunkSettings settings;
+    private ListOptim<Cube> cubes;
 
     private void Start()
     {
-        cubes = new ListOptim<GameObject>(1000);
+        cubes = new ListOptim<Cube>(1000);
         MeshRenderer[] cubesToAdd = GetComponentsInChildren<MeshRenderer>();
-        print(cubesToAdd.Length);
         for(int i =0; i < cubesToAdd.Length; ++i)
         {
-            cubes.Add(cubesToAdd[i].gameObject);
+            cubes.Add(new Cube(cubesToAdd[i].gameObject));
         }
         ActiveAll(false);
         RandomizeCubeActive();
+        LifeGeneration();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.P))
         {
             ActiveAll(false);
             RandomizeCubeActive();
+            LifeGeneration();
+        }
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            LifeGeneration();
         }
     }
 
@@ -32,7 +39,14 @@ public class ChunkManager : MonoBehaviour
     {
         for(int i =0; i < cubes.Count; ++i)
         {
-            cubes[i].SetActive(isActive);
+            if (cubes[i].posY==0)
+            {
+                cubes[i].cube.SetActive(true);
+            }
+            else
+            {
+                cubes[i].cube.SetActive(isActive);
+            }
         }
     }
 
@@ -43,8 +57,90 @@ public class ChunkManager : MonoBehaviour
             int rand = Random.Range(1, 101);
             if(rand>=50)
             {
-                cubes[i].SetActive(true);
+                cubes[i].cube.SetActive(true);
             }
         }
     }
+
+    private void LifeGeneration()
+    {
+        for (int j = 0; j < settings.loopCount; ++j)
+        {
+            for (int i = 0; i < cubes.Count; ++i)
+            {
+                int neighbor = NeighborCount(cubes[i].posX, cubes[i].posY, cubes[i].posZ);
+                if (neighbor == settings.neighborCountStayAlive)
+                {
+                    cubes[i].cube.SetActive(true);
+                }
+                else if (neighbor == settings.neighborCountNothingChange)
+                {
+                    continue;
+                }
+                else
+                {
+                    cubes[i].cube.SetActive(false);
+                }
+            }
+        }
+    }
+
+    private int NeighborCount(int posX, int posY, int posZ)
+    {
+        int count = 0;
+        for(int x = -1; x < 2; ++x)
+        {
+            for(int y = -1; y < 2; ++y)
+            {
+                for(int z = -1; z < 2; ++z)
+                {
+                    if ((x==0&&y==0&&z==0))
+                    {
+                        //continue;
+                    }
+                    else
+                    {
+                        GameObject cube = GetCubeAtPosition(posX + x, posY + y, posZ + z);
+                        count += cube != null ? ( cube.gameObject.activeInHierarchy ? 1 : 0 ) : 0;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    private GameObject GetCubeAtPosition(int posX, int posY, int posZ)
+    {
+        GameObject result = null;
+        for (int i = 0; i < cubes.Count; ++i)
+        {
+            if(cubes[i].IsCurrentCube(posX,posY,posZ))
+            {
+                result = cubes[i].cube;
+            }
+        }
+        return result;
+    }
+
+    public struct Cube
+    {
+        public GameObject cube;
+        public int posX;
+        public int posY;
+        public int posZ;
+
+        public Cube(GameObject cube)
+        {
+            this.cube = cube;
+            posX = (int)cube.transform.localPosition.x;
+            posY = (int)cube.transform.localPosition.y;
+            posZ = (int)cube.transform.localPosition.z;
+        }
+
+        public bool IsCurrentCube(int x, int y, int z)
+        {
+            return posX == x && posY == y && posZ == z;
+        }
+    }
+
 }
---
+++ Assets/Scripts/ScriptableObjects/ChunkSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Blame/SpawnerSettings")]
+public class ChunkSettings : ScriptableObject
+{
+
+    [Header("Algo life settings")]
+    public int loopCount = 2;
+    public int neighborCountStayAlive = 3;
+    public int neighborCountNothingChange = 2;
+    //public Vector2 neighborCountToDie;
+}
